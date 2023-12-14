9cfa2a28bd468d868299e1d6956b5f2daaffa1dd
SimoneRimedio <rimediosimone1@gmail.com>
2023-12-14 10:31:41 +0100

    Maeads

--- public/script/ClientCounter.js
+++
@@ -1,7 +0,0 @@
-// Creiamo un div per il contatore dei punti
-const ClientsCounter = document.createElement("div");
-ClientsCounter.setAttribute("valore", 0);
-ClientsCounter.setAttribute("name", "B");
-ClientsCounter.setAttribute("class", "ClientsCounter");
-
-document.body.appendChild(ClientsCounter);
