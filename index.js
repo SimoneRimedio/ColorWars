05cb79fb7dba36e502539bb121874eb861671ae9
SimoneRimedio <rimediosimone1@gmail.com>
2023-12-19 16:12:08 +0100

    ritorno a prima di express

--- index.js
+++ index.js
@@ -1,23 +1,21 @@
-const express = require("express");
+// index.js
+const http = require("http");
 const { Server } = require("ws");
-const { join } = require("path");
+const { handleRequest } = require("./server/routes");
 const { handleConnection } = require("./server/websocketHandlers");
 
-const PORT = 3000;
+// Creazione del server HTTP
+const server = http.createServer(handleRequest);
 
-const app = express();
-const ws_server = new Server({ port: PORT });
-
-app.get("/", (req, res) => {
-  app.use(express.static(join(__dirname, "client/css")));
-  app.use(express.static(join(__dirname, "client/utils")));
-  res.sendFile(join(__dirname, "client", "index.html"));
-});
+// Creazione del server WebSocket
+const ws_server = new Server({ server });
 
 ws_server.on("connection", (ws) => {
   handleConnection(ws, ws_server);
 });
 
-app.listen(PORT, () => {
+// Avvio del server HTTP
+const PORT = 3000;
+server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
